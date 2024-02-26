bfc507bf747c953eb81a52422ad21ce1762d14be
Bob Wakefield <bob@cockleburr.net>
2024-02-26 13:15:24 -0600

    Initial Commit

---
+++ WindowSceneReaderDemo/ContentView.swift
@@ -0,0 +1,28 @@
+// ContentView.swift
+//
+// Created by Bob Wakefield on 2/26/24.
+// for WindowSceneReaderDemo
+//
+// Using Swift 5.0
+// Running on macOS 14.3
+//
+// 
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WindowSceneReaderDemo/WindowSceneReaderDemoApp.swift
@@ -0,0 +1,21 @@
+// WindowSceneReaderDemoApp.swift
+//
+// Created by Bob Wakefield on 2/26/24.
+// for WindowSceneReaderDemo
+//
+// Using Swift 5.0
+// Running on macOS 14.3
+//
+// 
+//
+
+import SwiftUI
+
+@main
+struct WindowSceneReaderDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
